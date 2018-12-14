5e70ba872213c46739c0a01639835e4268d674a9
Alexander Kaloshin <alexander.kaloshin@gmail.com>
2018-12-14 22:01:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening lol');
+  console.log('Example app listening');
 });
